a154b59ebd929591950489f7256721402cd91620
neon1024 <robertscumnpu@gmail.com>
2021-09-01 11:46:51 +0300

    moving files

---
+++ functions.js
@@ -0,0 +1,113 @@
+var x = "Hello";
+
+console.log(x.length);
+
+x.slice(2, 4);
+
+console.log(x);
+
+x.replace("Good bye");
+
+console.log(x);
+
+x.toUpperCase;
+
+console.log(x);
+
+x.toLowerCase(x);
+
+console.log(x);
+
+x.trim;
+
+console.log(x);
+
+x.padStart(5)   // --> 00000Hello
+
+console.log(x);
+
+x.padEnd(5);    // --> Hello00000
+
+console.log(x);
+
+x.charAt(0)
+
+console.log(x);
+
+x.charCodeAt(x);
+
+console.log(x);
+
+var y = 101;
+
+// NaN --> not a number
+
+// Infinity
+
+y.toString;
+
+console.log(y);
+
+y.toExponential
+
+console.log(y);
+
+y.toFixed;
+
+console.log(y);
+
+y.toPrecision;
+
+console.log(y);
+
+y.valueOf;
+
+console.log(y);
+
+Number("101");  // --> 101
+
+Number("Today is Wednesday");   // --> NaN
+
+Number("1 text");   // --> NaN
+
+parseInt("5g Sugar");   // --> 5
+
+parseInt("5g Sugar, 20g Amareto");  // --> 5
+
+//parseInt returns the first number
+
+y.MAX_VALUE;
+
+y.MIN_VALUE;
+
+var z = {"Someone", undefined, 210, True, "chachacha"};
+
+toString
+
+join('*'); // --> Someone * 210 * True * chachacha
+
+z.pop();    // --> deletes the last element
+
+z.shift();  // --> deletes the first element
+
+z.unshift();    // --> adds an element in the first position
+
+z.push();   // --> adds an element to the last position
+
+z.length(); // --> returns the length of the array
+
+z.delete(1);  // --> deletes the n(first) element
+
+z.splice(1, 3);
+
+var w = {1, 2, 3, 4};
+
+concat(z, w);   // --> concatenates the two arrays
+
+z.slice(1, 3);
+
+z.sort();
+
+z.reverse();
+
+
---
+++ script.js
@@ -0,0 +1,23 @@
+
+var answer =  document.getElementById("result");
+
+function sumOfTwoNumbers() {
+    answer.innerHTML = 399893 + 1290;
+}
+
+function differentionOfTwoNumbers() {
+    answer.innerHTML = 9293 - 1892;
+}
+
+function multiplicationOfTwoNumbers() {
+    answer.innerHTML = 9291 * 3402;
+}
+
+function divisionOfTwoNumbers() {
+    answer.innerHTML = 892 / 28;
+}
+
+function moduloOfTwoNumbers() {
+    answer.innerHTML =  2901 % 929;
+}
+
